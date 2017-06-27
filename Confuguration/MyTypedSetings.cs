f247aa8e1f23627e02cddb090ea93c747d8ea9c8
wegal <kona74@gmx.at>
2017-06-27 09:39:38 +0200

    Add project files.

---
+++ Confuguration/MyTypedSetings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Confuguration
+{
+
+    public struct Point
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+
+   public class MyTypedSetings
+    {
+        public Point TopLeft { get; set; }
+        public string Title { get; set; }
+
+
+    }
+}
---
+++ Confuguration/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Primitives;
+using System.IO;
+namespace Confuguration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SetupConfiguration(args);
+            ReadConfiguration();
+        }
+
+        private static void ReadConfiguration()
+
+        {
+
+            string setting1 = Configuration.GetSection("MyConfig")["Setting1"];
+
+            Console.WriteLine(setting1);
+
+
+
+
+
+            string defaultConnection = Configuration.GetConnectionString("Wartezeit");
+
+            Console.WriteLine(defaultConnection);
+
+
+
+            MyTypedSetings setting = new MyTypedSetings();
+
+            Configuration.GetSection("MyTypedSetting").Bind(setting);
+
+            Console.WriteLine($"{setting.Title} {setting.TopLeft.X} {setting.TopLeft.Y}");
+
+
+
+            //string secret = Configuration["secret1"];
+
+            //Console.WriteLine($"this is a secret: {secret}");
+
+        }
+
+
+        static void SetupConfiguration(string[] args)
+        {
+            IConfigurationBuilder builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+              .AddJsonFile("apsettings.json")
+
+                .AddJsonFile("alternative.json", optional: true)
+
+                //.AddEnvironmentVariables()
+
+                .AddCommandLine(args);
+
+
+
+            Configuration = builder.Build();
+
+
+
+        }
+
+
+        public static IConfigurationRoot Configuration { get; private set; }
+
+    }
+}
